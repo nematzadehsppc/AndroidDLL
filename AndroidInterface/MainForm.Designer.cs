0f9d2ee244ec239500982f35013e8cb4c4afa5a6
Behzad Nematzadeh <b_nematzadeh@sppc.co.ir>
2019-05-26 17:58:28 +0430

    create AndroidDLL

---
+++ AndroidInterface/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AndroidInterface
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(136, 30);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(75, 23);
+            this.btnRequest.TabIndex = 0;
+            this.btnRequest.Text = "Request";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            this.btnRequest.Click += new System.EventHandler(this.btnRequest_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 96);
+            this.Controls.Add(this.btnRequest);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRequest;
+    }
+}
+
---
+++ AndroidInterface/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndroidInterface
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            CustomerInterface.CustomerInfo customerInfo = new CustomerInterface.CustomerInfo();
+
+            customerInfo.IP = new InArgument<string>("130.185.76.7");
+            customerInfo.Port = new InArgument<int>(9020);
+            customerInfo.Database = new InArgument<string>("TEST");
+            customerInfo.Version = new InArgument<string>("V3.0");
+            customerInfo.UserId = new InArgument<int>(9);
+            customerInfo.FPId = new InArgument<int>(1);
+            WorkflowInvoker.Invoke<string>(customerInfo);
+        }
+    }
+}
